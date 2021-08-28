afdd91f3a6422e21e0ef7f6b311ca55123dd91e3
Ririgirl <shatteredglass1@mail.ru>
2021-08-28 22:00:52 +0300

    second commit

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    form = PostAdminForm
+    #save_as = True #сохранить как новый объект
+    list_display = ('id', 'title', 'author', 'created_at', 'category', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+    list_filter = ('category',)
+    save_on_top = True
+    readonly_fields = ('views', 'created_at', 'get_photo')
+    fields = ('title', 'slug', 'author', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
+
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        return '-'
+
+    get_photo.short_description = 'Фото'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Post, PostAdmin)
+
+admin.site.site_title = 'Управление блогом'
+admin.site.site_header = 'Управление блогом'
--- siteblog/blog/models.py
+++ siteblog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -10,6 +11,12 @@ class Category(models.Model):
 
     class Meta:
         ordering = ['title']
+        verbose_name = 'Категория (ю)'
+        verbose_name_plural = 'Категории'
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug':self.slug})
+
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
@@ -20,20 +27,27 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ['title']
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
     slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=100, verbose_name='Автор')
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Опубликовано')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     views = models.IntegerField(default=0, verbose_name='Количество просмотров')
-    category = models.ForeignKey(Category,on_delete=models.PROTECT, related_name='posts')
+    category = models.ForeignKey(Category,on_delete=models.PROTECT, related_name='posts', verbose_name='Категория')
     tags = models.ManyToManyField(Tag,blank=True, related_name='posts')
 
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug':self.slug})
+
     class Meta:
         ordering = ['-created_at']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
